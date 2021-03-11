1d532b27ede9fd0ff27560d37fbbab6b16ef66c4
Malo Bourgon <mbourgon@gmail.com>
2021-03-11 15:17:40 -0800

    Update to Matt F's Brewfile

--- darwin/homebrew.nix
+++ darwin/homebrew.nix
@@ -14,65 +14,131 @@
     "homebrew/services"
   ];
 
+  # TODO: Check whether these are in `nixpkgs`
+  homebrew.brews = [
+    "rbenv"
+    "trash"
+  ];
+
   homebrew.casks = [
+    # Development
     "atom"
-    "amethyst"
-    "arq"
-    "audio-hijack"
+    "dash"
+    "db-browser-for-sqlite"
+    "dbeaver-community"
+    "docker"
+    "font-firacode-nerd-font"
+    "font-jetbrainsmono-nerd-font-mono"
+    "gitup"
+    "growlnotify"
+    "insomnia"
+    "iterm2"
+    "java"  # required by dbeaver-community
+    "macvim"
+    "sublime-merge"
+    "vagrant"
+    "vimr"
+    "virtualbox"
+    "virtualbox-extension-pack"
+    "visual-studio-code"
+    "zerobranestudio"
+
+    # Crypto
+    "electron-cash"
+    "electrum"
+
+    # Hardware hacking
+    "arduino"
+    "freecad"
+    "meshlab"
+    "meshmixer"
+    "openscad-snapshot"
+    "prusaslicer"
+    "ultimaker-cura"
+
+    # Services from Homebrew
+    "markdown-service-tools"
+
+    # QuickLook plugins from Homebrew
+    "qlcolorcode"
+    "qlimagesize"
+    "qlmarkdown"
+    "qlstephen"
+    "quicklook-csv"
+    "quicklook-json"
+
+    # Other GUI apps
     "balenaetcher"
-    "camo-studio"
-    "cloudflare-warp"
-    "displaycal"
+    "calibre"
+    "dropbox"
+    "epichrome"
+    "etrecheckpro"
     "firefox"
+    "freemind"
+    "google-backup-and-sync"
     "google-chrome"
-    "google-drive"
+    "google-drive-file-stream"
+    "marshallofsound-google-play-music-player"
     "gpg-suite"
+    "grandperspective"
     "hammerspoon"
-    "hey"
-    "i1profiler"
+    "imageoptim"
+    "inkscape"
+    "karabiner-elements"
     "keybase"
-    "launchbar"
-    "parallels"
-    "protonvpn"
+    "keycue"
+    "launchcontrol"
+    "libreoffice"
+    "nvalt"
+    "quicksilver"
+    "rowanj-gitx"
+    "setapp"
     "signal"
+    "skyfonts"
     "skype"
+    "slack"
     "steam"
-    "superhuman"
-    "tastyworks"
+    "suspicious-package"
     "tor-browser"
     "transmission"
-    "transmit"
     "typinator"
-    "visual-studio-code-insiders"
+    "ubersicht"
+    "unison"
+    "viscosity"
     "vlc"
-    "yubico-authenticator"
-    "yubico-yubikey-manager"
-    "yubico-yubikey-personalization-gui"
+    "xquartz"
+    "yed"
+    "yt-music"
+
+    # Other
+    "logitech-unifying"
+    "onyx"
   ];
 
   homebrew.masApps = {
-    "1Blocker" = 1107421413;
     "1Password" = 1333542190;
-    "Accelerate for Safari" = 1459809092;
-    DaisyDisk = 411643860;
-    "Dark Mode for Safari" = 1397180934;
+    "AVG Cleaner" = 667434228;
+    Calca = 635758264;
+    "Contacts Sync For Google Gmail" = 451691288;
     Deliveries = 924726344;
-    Fantastical = 975937182;
+    eDrawings = 1209754386;
+    Evernote = 406056744;
+    Gapplin = 768053424;
+    GarageBand = 682658836;
+    Growl = 467939042;
+    "Icon Slate" = 439697913;
+    iMovie = 408981434;
     Keynote = 409183694;
+    Kindle = 405399194;
     "LG Screen Manager" = 1142051783;
-    MindNode = 1289197285;
+    "Lights Switch" = 1181873676;
     Numbers = 409203825;
     Pages = 409201541;
-    Patterns = 429449079;
-    Pixelmator = 407963104;
     "Pixelmator Pro" = 1289583905;
+    Singlemizer = 410263119;
     Slack = 803453959;
-    SiteSucker = 442168834;
-    "Things 3" = 904280696;
-    "Type Fu" = 509818877;
-    Ulysses = 1225570693;
-    Vimari = 1480933944;
-    "WiFi Explorer" = 494803304;
+    Typeeto = 970502923;
+    "The Unarchiver" = 425424353;
     Xcode = 497799835;
   };
 }
